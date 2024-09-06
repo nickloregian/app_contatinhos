6e09abfb1387f6535581b814dd412ee3ff81aa22
Nicolly Lima <nicollysenai29@gmail.com>
2024-09-06 16:20:49 -0300

    adicionando padrão de cores

---
+++ src/theme/colors.ts
@@ -0,0 +1,8 @@
+import { blue } from "react-native-reanimated/lib/typescript/reanimated2/Colors";
+
+export const colors = {
+    white: "#FFFFFF",
+    black: "#000000",
+    
+    blue: "#4442F0",
+}
